fad0ad571139073aef3088dcf302f05065b932e0
Ayush <ayush.up105@gmail.com>
2023-09-01 05:32:59 +0530

    1st commit

--- createServer.js
+++ createServer.js
@@ -1,12 +1,12 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-    res.write("<b>Ayush loves Diksha</b><hr>");
+    res.write("<b>Ayush </b><hr>");
     res.end();
 }).listen(1000); 
 
 const datacontrol=(req , res)=>{
-    res.write("<b>Ayush loves Anamika</b><hr>");
+    res.write("<b>Ayush </b><hr>");
     res.end();
 }
 http.createServer(datacontrol).listen(5000);
